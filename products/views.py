3e969f300ac8e0efabb930ffa066a4510741e483
GaryS007 <chattomeh@hotmail.com>
2023-11-24 11:41:47 +0000

    Add product detail page and update products page mobile styling

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from .models import Product
 
 def all_products(request):
@@ -13,4 +13,18 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ 
+    A view to show individual product details
+    """
+
+    products = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/product_detail.html', context)
 
