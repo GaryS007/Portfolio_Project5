bc5ef8d60b761dff3389d1a40de37e14f6c76cf9
GaryS007 <chattomeh@hotmail.com>
2023-11-27 14:42:31 +0000

    Added add to bag functionality

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_checkout, name='view_checkout')
+    path('', views.view_checkout, name='view_checkout'),
+    path('add/<item_id>/', views.add_to_checkout, name='add_to_checkout'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_checkout(request):
@@ -7,3 +7,22 @@ def view_checkout(request):
     """
 
     return render(request, 'checkout/checkout.html')
+
+
+def add_to_checkout(request, item_id):
+    """ 
+    Add a quantity of the specified product to the shopping cart
+    """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
