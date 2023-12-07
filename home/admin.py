1e1418c6af4ef97dc729d702969b846a34bbacec
GaryS007 <chattomeh@hotmail.com>
2023-12-07 17:03:43 +0000

    Fix search, update color scheme

--- home/admin.py
+++ home/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import ContactUs
-from djang_summernote.admin import SummernoteModelAdmin
+from django_summernote.admin import SummernoteModelAdmin
 
 
-@admin.regster(ContactUs)
+@admin.register(ContactUs)
 class ContactUsAdmin(admin.ModelAdmin):
     """Represents the ContactUs model in the Django Admin Interface"""
-    list_display = (__all)
-    list_filter = ('sent_on')
+    list_display = ('name', 'email', 'message', 'sent_on',)
+    list_filter = ('sent_on',)
     summernote_fields = ('message',)
